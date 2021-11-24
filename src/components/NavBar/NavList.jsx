8dd6a8fa6a6559fa38dceeabb41135c0ed6fe13f
muhammadfauzulhanif16 <muhammadfauzulhanif16@outlook.com>
2021-11-24 12:53:15 +0700

    create nav list for navbar

---
+++ src/components/NavBar/NavList.jsx
@@ -0,0 +1,7 @@
+export const NavList = [
+  { title: "Home", isHot: false },
+  { title: "About", isHot: false },
+  { title: "Promotions", isHot: true },
+  { title: "Blogs", isHot: false },
+  { title: "Contact Us", isHot: false },
+];
