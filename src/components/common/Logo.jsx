875f2b76e1e774b790f14bd83a4d38f1b389c4c8
muhammadfauzulhanif16 <muhammadfauzulhanif16@outlook.com>
2021-11-24 13:20:36 +0700

    create logo component

---
+++ src/components/common/Logo.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import ElemesIndonesia from "../../images/Elemes Indonesia.svg";
+
+export const Logo = () => {
+  return <img src={ElemesIndonesia} alt="Elemes Indonesia" />;
+};
