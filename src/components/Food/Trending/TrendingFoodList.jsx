3e001220192e1873c1a2c72359f9cf4ddd330ffd
muhammadfauzulhanif16 <muhammadfauzulhanif16@outlook.com>
2021-11-24 12:58:10 +0700

    create trending food list

---
+++ src/components/Food/Trending/TrendingFoodList.jsx
@@ -0,0 +1,67 @@
+import PizzaPaperoni from "../../../images/foods/Pizza Paperoni.svg";
+import PizzaMeat from "../../../images/foods/Pizza Meet.svg";
+import DonerKebab from "../../../images/foods/Doner Kebab.svg";
+import SalmonRoll from "../../../images/foods/Salmon Roll.svg";
+import CupcakeChoco from "../../../images/foods/Cupcake Choco.svg";
+import DoughnutMilk from "../../../images/foods/Doughnut Milk.svg";
+import DoughnutUnicorn from "../../../images/foods/Doughnut Unicorn.svg";
+import KathiKebab from "../../../images/foods/Kathi Kebab.svg";
+
+export const TrendingFoodList = [
+  {
+    image: `${PizzaPaperoni}`,
+    name: "Pizza Paperoni",
+    category: "Pizza",
+    rate: 4,
+    bgColor: "bg-aqua-e6f3f5",
+  },
+  {
+    image: `${PizzaMeat}`,
+    name: "Pizza Meat",
+    category: "Pizza",
+    rate: 3,
+    bgColor: "bg-aqua-e6f3f5",
+  },
+  {
+    image: `${DonerKebab}`,
+    name: "Doner Kebab",
+    category: "Kebab",
+    rate: 5,
+    bgColor: "bg-white-eaeefa",
+  },
+  {
+    image: `${SalmonRoll}`,
+    name: "Salmon Roll",
+    category: "Salmon",
+    rate: 4,
+    bgColor: "bg-white-f9eef3",
+  },
+  {
+    image: `${CupcakeChoco}`,
+    name: "Cupcake Choco",
+    category: "Cupcake",
+    rate: 4,
+    bgColor: "bg-white-f0feeb",
+  },
+  {
+    image: `${DoughnutMilk}`,
+    name: "Doughnut Milk",
+    category: "Doughnut",
+    rate: 5,
+    bgColor: "bg-lime-f3f7d9",
+  },
+  {
+    image: `${DoughnutUnicorn}`,
+    name: "Doughnut Unicorn",
+    category: "Doughnut",
+    rate: 4,
+    bgColor: "bg-lime-f3f7d9",
+  },
+  {
+    image: `${KathiKebab}`,
+    name: "Kathi Kebab",
+    category: "Kebab",
+    rate: 4,
+    bgColor: "bg-white-eaeefa",
+  },
+];
