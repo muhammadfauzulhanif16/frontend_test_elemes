894ffb31f31cd77b168dc3a9cdd5870f5c9b376f
muhammadfauzulhanif16 <muhammadfauzulhanif16@outlook.com>
2021-11-24 12:58:19 +0700

    create category food list

---
+++ src/components/Food/Category/CategoryFoodList.jsx
@@ -0,0 +1,68 @@
+import Cupcake from "../../../images/icons/foods/Cupcake.svg";
+import Pizza from "../../../images/icons/foods/Pizza.svg";
+import Kebab from "../../../images/icons/foods/Kebab.svg";
+import Salmon from "../../../images/icons/foods/Salmon.svg";
+import Doughnut from "../../../images/icons/foods/Doughnut.svg";
+
+export const CategoryFoodList = [
+  {
+    image: `${Cupcake}`,
+    name: "Cupcake",
+    items: 4,
+    bgColor: "bg-aqua-e6f3f5",
+  },
+  {
+    image: `${Pizza}`,
+    name: "Pizza",
+    items: 3,
+    bgColor: "bg-aqua-e6f3f5",
+  },
+  {
+    image: `${Kebab}`,
+    name: "Kebab",
+    items: 5,
+    bgColor: "bg-white-eaeefa",
+  },
+  {
+    image: `${Salmon}`,
+    name: "Salmon",
+    items: 4,
+    bgColor: "bg-white-f9eef3",
+  },
+  {
+    image: `${Doughnut}`,
+    name: "Doughnut",
+    items: 4,
+    bgColor: "bg-white-f0feeb",
+  },
+  {
+    image: `${Cupcake}`,
+    name: "Cupcake",
+    items: 4,
+    bgColor: "bg-aqua-e6f3f5",
+  },
+  {
+    image: `${Pizza}`,
+    name: "Pizza",
+    items: 3,
+    bgColor: "bg-aqua-e6f3f5",
+  },
+  {
+    image: `${Kebab}`,
+    name: "Kebab",
+    items: 5,
+    bgColor: "bg-white-eaeefa",
+  },
+  {
+    image: `${Salmon}`,
+    name: "Salmon",
+    items: 4,
+    bgColor: "bg-white-f9eef3",
+  },
+  {
+    image: `${Doughnut}`,
+    name: "Doughnut",
+    items: 4,
+    bgColor: "bg-white-f0feeb",
+  },
+];
